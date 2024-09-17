3f1d4e1612339e6efb1a14773f49ca2eaa93deb3
webaldar <webaldar@gmail.com>
2024-09-17 08:28:24 +0300

    refactor code

--- src/s2-homeworks/hw02/HW2.tsx
+++ src/s2-homeworks/hw02/HW2.tsx
@@ -36,13 +36,10 @@ const defaultAffairs: AffairType[] = [ // need to fix any
 
 // pure helper functions
 export const filterAffairs = (affairs: AffairType[], filter: FilterType): AffairType[] => { // need to fix any
-    if (filter === 'all') {
-        return affairs
-    }
-    return affairs.filter(el => el.priority === filter)
+    return (filter === 'all') ? affairs : affairs.filter(el => el.priority === filter)
     // need to fix
 }
-export const deleteAffair = (affairs: AffairType[], _id: number): any => { // need to fix any
+export const deleteAffair = (affairs: AffairType[], _id: number): AffairType[] => { // need to fix any
 
     return affairs.filter(el => el._id !== _id)
     // need to fix
