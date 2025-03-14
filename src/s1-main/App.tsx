b45bf55aa26e1b97868aed909c791e29191a15e2
Webaldar <webaldar@gmail.com>
2025-03-14 18:00:54 +0300

    HW5 delete unused import

--- src/s1-main/App.tsx
+++ src/s1-main/App.tsx
@@ -1,8 +1,5 @@
 import React from 'react'
 import s from './App.module.css'
-import HW1 from '../s2-homeworks/hw01/HW1'
-import HW2 from "../s2-homeworks/hw02/HW2";
-import HW3 from "../s2-homeworks/hw03/HW3";
 import HW5 from "../s2-homeworks/hw05/HW5";
 
 function App() {
